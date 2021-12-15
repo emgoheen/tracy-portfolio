0c224f674adeae62f4896eb0a98a8b452ecb70c8
Emily Tracy <emgoheen@yahoo.com>
2021-12-15 11:07:14 -0800

    Cleaned up styles, added core and shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,           // Singleton objects (services, components that are loaded only once, etc.)
+    SharedModule          // Shared (multi-instance) objects
   ],
   providers: [],
   bootstrap: [AppComponent]
